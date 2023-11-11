4730f00b84f40b80347e4bc7d57d2541c25447af
Nurkhat Muratkan <nurhatmurathan001@gmail.com>
2023-11-11 22:31:49 +0600

    feat: register user sample 2

--- api/serializers.py
+++ api/serializers.py
@@ -8,3 +8,7 @@ class UserPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
