affe08c5117029c661331349c5e4676e227e6ce3
AferDust <aferdust27@gmail.com>
2023-11-11 22:42:22 +0600

    feat: adding gpt question/answer endpoint

--- api/serializers.py
+++ api/serializers.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 
 class GPTChatSerializer(serializers.Serializer):
-    message = serializers.CharField(allow_null=False, min_length=5)
+    message = serializers.CharField(allow_null=False)
--- api/views.py
+++ api/views.py
@@ -1,12 +1,32 @@
+import openai
 from django.shortcuts import render
 from dotenv import load_dotenv
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import GPTChatSerializer
 
-load_dotenv()
+client = openai.OpenAI()
+
 
 class GPTChatAPIView(APIView):
     def post(self, request):
-        pass
+        try:
+            serializer = GPTChatSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+
+            user_message = serializer.data.get('message')
+
+            response = client.chat.completions.create(
+                model='gpt-3.5-turbo',
+                messages=[
+                        {"role": "system", "content": "You are a helpful assistant."},
+                        {"role": "user", "content": user_message}
+                    ]
+                )
+
+            response_message = response.choices[0].message.content
+            return Response(response_message, status=status.HTTP_200_OK)
+        except Exception as exception:
+            return Response(data={'message': str(exception)}, status=status.HTTP_400_BAD_REQUEST)
