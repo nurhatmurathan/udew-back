95a49fedad4e4d9caf0d4617f0531aa4f49f3ee5
AferDust <aferdust27@gmail.com>
2024-01-26 16:12:54 +0600

    docs: change code structure

--- api/serializers/gpt.py
+++ api/serializers/gpt.py
@@ -1,20 +1,7 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
 class GPTChatSerializer(serializers.Serializer):
     question = serializers.CharField(allow_null=False)
     answer = serializers.CharField(allow_null=False)
-
-
-class UserPostSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password']
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
 
