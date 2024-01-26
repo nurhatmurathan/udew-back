985a0fee1fb095e213bd2f6d6e4583704747d4e4
Nurkhat Muratkan <nurhatmurathan001@gmail.com>
2024-01-26 19:07:17 +0600

    feat: model multilingual, image

--- api/models.py
+++ api/models.py
@@ -1,8 +1,19 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # class Project(models.Model):
 #     user = models.OneToOneField(User, on_delete=models.CASCADE)
 #     name = models.CharField()
 
+
+class MultilingualText(models.Model):
+    en = models.TextField()
+    ru = models.TextField()
+    kz = models.TextField()
+
+
+class ImageUrls(models.Model):
+    preview = models.URLField(max_length=200)
+    pc = models.URLField(max_length=200)
+    tablet = models.URLField(max_length=200)
+    modile = models.URLField(max_length=200)
